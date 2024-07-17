f37f9c438fecb6c8a2b1a1f4e077c4163a5c38e3
n3rd-vignesh <65106007+n3rd-vignesh@users.noreply.github.com>
2024-07-17 15:10:51 +0530

    Create main_server.cpp

---
+++ main_server.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define PORT 8080
+
+void handleClient(int client_socket) {
+    char buffer[1024] = {0};
+    int valread = read(client_socket, buffer, 1024);
+    if (valread > 0) {
+        std::cout << "Received data from client:\n" << buffer << std::endl;
+
+        // Vulnerable function (buffer overflow)
+        char response[256];
+        strcpy(response, buffer); // Potential buffer overflow
+
+        // Send response back to client
+        send(client_socket, response, strlen(response), 0);
+    }
+}
+
+int main() {
+    int server_fd, client_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Forcefully attaching socket to the port 8080
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    while (true) {
+        if ((client_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+
+        // Handle each client connection in a separate thread or process
+        handleClient(client_socket);
+        close(client_socket);
+    }
+    return 0;
+}
